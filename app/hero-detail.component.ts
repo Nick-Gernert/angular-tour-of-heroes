2c5ebff2701ce7beb5b3c20c133e5c74e1ad4594
Nick Gernert <nickgernert@gmail.com>
2016-05-24 15:38:09 -0400

    Finished the tour

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,18 +1,43 @@
-import { Component, Input } from '@angular/core';
-import { Hero } from './hero';
-
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { RouteParams } from '@angular/router-deprecated';
+import { Hero }        from './hero';
+import { HeroService } from './hero.service';
 @Component({
   selector: 'my-hero-detail',
-  template: `<div *ngIf="hero">
-                <h2>{{hero.name}} details!</h2>
-                <div><label>id: </label>{{ hero.id }}</div>
-                <div>
-                    <label>name: </label>
-                    <input [(ngModel)]="hero.name" placeholder="name"/>
-                </div>
-             </div>`
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: ['app/hero-detail.component.css']
 })
-export class HeroDetailComponent {
-    @Input()
-    hero: Hero;
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+  @Output() close = new EventEmitter();
+  error: any;
+  navigated = false; // true if navigated here
+  constructor(
+    private _heroService: HeroService,
+    private _routeParams: RouteParams) {
+  }
+  ngOnInit() {
+    if (this._routeParams.get('id') !== null) {
+      let id = +this._routeParams.get('id');
+      this.navigated = true;
+      this._heroService.getHero(id)
+          .then(hero => this.hero = hero);
+    } else {
+      this.navigated = false;
+      this.hero = new Hero();
+    }
+  }
+  save() {
+    this._heroService
+        .save(this.hero)
+        .then(hero => {
+          this.hero = hero; // saved hero, w/ id if new
+          this.goBack(hero);
+        })
+        .catch(error => this.error = error); // TODO: Display error message
+  }
+  goBack(savedHero: Hero = null) {
+    this.close.emit(savedHero);
+    if (this.navigated) { window.history.back(); }
+  }
 }
